df9c8b834944199f6a9ca31633ac160ce45565dd
migsandia <migsandia@gmail.com>
2022-03-09 16:12:05 +0100

    Version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,35 @@
+
+// import * as React from 'react';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+
+
+
+describe('ProductImage', () => { 
+
+    test('debe de mostrar el componente correctamente con la imagen personalizada', () => { 
+
+        const wrapper = renderer.create(
+            <ProductImage img="http://hola.jpg" className="custom-class"/>
+        );
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+    test('debe de mostrar el componente con la imagen del producto', () => {
+
+        const wrapper = renderer.create(
+            <ProductCard product={ product2 }>
+                {
+                    () => (
+                        <ProductImage />
+                    )
+                }
+            </ProductCard>
+        );
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+})
---
+++ test/data/products.ts
@@ -0,0 +1,11 @@
+export const product1 = {
+    id: '1',
+    title: 'Coffe Mug - sin imagen',
+    //img: './coffee-mug.png'
+}
+
+export const product2 = {
+    id: '2',
+    title: 'Coffe Mug - con imagen',
+    img: './coffee-mug.png'
+}
